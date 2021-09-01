43889f0767fe357cbbb3edd3122b1765b442ff33
C0ffee Bandit <c0ffee_bandit@protonmail.com>
2021-08-31 22:03:34 -0500

    Did some tweaking while class was going on.

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+let welcome = document.getElementById("login-welcome");
+welcome.innerHTML = "Welcome, Guest!";
+
+// hide food not found
+let fnf = document.getElementById("foodNotFound");
+fnf.style.display = "none";
+
+// TODO: team-front needs to slide down the distance of the team-back element behind it
+
