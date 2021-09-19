8ef8b7b34417c402b2732ea5e50573dde84fbf90
C0ffee Bandit <c0ffee_bandit@protonmail.com>
2021-09-19 17:00:09 -0500

    Template Restructure

---
+++ scripts/README.md
@@ -0,0 +1,63 @@
+# CSS.js
+
+A library to quickly create CSS and animations with JavaScript without CSS files.
+
+Test with
+```html
+<div class="moro"/> 
+```
+Then write
+```javascript
+css().bind(".moro", {
+  width : "100px",
+  height:"100px",
+  "background-color" : "blue"
+});
+
+css().bind(".moro:hover", {
+        "background" : "linear-gradient(#666, #333)"
+    });
+
+```
+[Code above in Codepen](http://codepen.io/teroktolonen/full/wzvkKG)
+
+## Animation
+
+Having above example, but adding a class
+```html
+<div class="moro viewIn"/> 
+```
+
+Then:
+
+```javascript
+var inPosition = {
+    "transform" : "translate(0,0)",
+    
+};
+var outPosition = {
+    "transform" : "translate(-130px,0px) rotate(90deg)",
+    "opacity" : 0
+};
+
+css().animation("viewIn", {
+    duration : "1s",
+    "iteration-count" : 1,
+},  outPosition,  inPosition); 
+```
+
+[Try in CodePen](http://codepen.io/teroktolonen/full/LRYvGr)
+
+## Gradient animation
+
+Simple gradient animation is possible if you have two gradients and animate their opacity
+
+[Try in CodePen](http://codepen.io/teroktolonen/full/rraaZG/)
+
+## Browser compatibility testing
+
+Feedback is welcome. On the desktop browsers the limit goes on IE9, basic features work but not animations. IE10+ also animations work, so that's approximately the browser compatibility limit on desktop. On mobile at least Android 4.1 and 2.3 seems to work. On 2.2 the Codepen did not work :(. iOS 5 & 4 & 3 seem to work.
+
+## License
+
+MIT. Or whatever you need.
--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,88 @@
 let welcome = document.getElementById("login-welcome");
 welcome.innerHTML = "Welcome, Guest!";
+welcome.addEventListener("click",welcomeClick);
+let shareButton = document.getElementById("share");
+let loginButton = document.getElementById("login");
+let logoutButton = document.getElementById("logout");
+let registerButton = document.getElementById("register");
+shareButton.addEventListener("click",shareClick);
+loginButton.addEventListener("click",loginClick);
+logoutButton.addEventListener("click",logoutClick);
+registerButton.addEventListener("click",registerClick);
+function welcomeClick() {
+	// populate and show user profile screen?
+	console.log("welcome clicked");
+}
+function shareClick(){
+  // unhide and focus share screen
+  console.log( "share clicked");
+}
+function loginClick(){
+  // unhide and focus login screen
+  console.log("login clicked");
+}
+function logoutClick(){
+  // logout user, change welcome
+  console.log("logout clicked");
+}
+function registerClick(){
+  // unhide and focus register screen
+  console.log("register clicked");
+}
 
 // hide food not found
 let fnf = document.getElementById("foodNotFound");
 fnf.style.display = "none";
 
 // TODO: team-front needs to slide down the distance of the team-back element behind it
+// figure this out on the fly or it looks weird ... 
+// pull array of 'our-team-main', lookup height of 'team-back' and set it to the slide of 'team-front'
+// ug fails ... tehy all need their own class ... lets try it a different way
+let teamMain = document.getElementsByClassName("our-team-main");
+let average = 0;
+for( const div of teamMain ){
+	// console.log( div );
+  // div.addEventListener("onmouseover", moveDiv());
+  // div.addEventListener("onmouseout", fixDiv());
+	// let height = div.children[1].getBoundingClientRect().height;
+	// average += height;
+}
+// console.log("-" + (average / teamMain.length).toFixed(0) + "px");
+function moveDiv(div){
+  // console.log( "moveDiv", div );
+  // let newTop = 250;
+  // console.log(
+	// 	"moveDiv fired",
+	// 	div,
+	// 	"- " + div.children[0].style.top + " -",
+	// 	newTop + "px"
+	// );
+  // console.log(div.children[0].style);
+  // return;
+  // var newTop = div.children[0].style.top;
+  // newTop = parseInt(newTop);
+  // newTop -= 200;
+  // div.children[0].style.top = "250px";
+  // div.children[0].style.transition = "all 1s ease 1s";
+}
+function fixDiv(div){
+  // console.log( "fixDiv", div );
+	// let newTop = 0;
+  // console.log(
+	// 	"fixDiv fired",
+	// 	div,
+	// 	"- " + div.children[0].style.top + " -",
+	// 	newTop + "px"
+	// );
+  // console.log( div.children[0].style );
+  // return;
+  // var newTop = div.children[0].style.top;
+	// newTop = parseInt(newTop);
+	// newTop += 200;
+	// div.children[0].style.top = "";
+  // div.children[0].style.transition = "all 1s ease 1s";
+  // console.log("fixDiv fired", div, div.children[0].style.top, newTop + "px");
+}
 
+// login (basic with user/pass) -> save token
+// to use token -> put in API auth header with key "Authorization"
