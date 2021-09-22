8e7ade23b9dedb51b47780e7f2accbc833494f3e
C0ffee Bandit <c0ffee_bandit@protonmail.com>
2021-09-22 11:19:07 -0500

    some refactor, login/logout functioning correct

--- scripts/app.js
+++ scripts/app.js
@@ -1,82 +1,40 @@
-let welcome = document.getElementById("login-welcome");
-welcome.innerHTML = "Welcome, Guest!";
-welcome.addEventListener("click",welcomeClick);
-let shareButton = document.getElementById("share");
-let loginButton = document.getElementById("login");
-let logoutButton = document.getElementById("logout");
-let registerButton = document.getElementById("register");
-shareButton.addEventListener("click",shareClick);
-loginButton.addEventListener("click",loginClick);
-logoutButton.addEventListener("click",logoutClick);
-registerButton.addEventListener("click",registerClick);
-function welcomeClick() {
-	// populate and show user profile screen?
-	console.log("nav welcome clicked");
-}
-function shareClick(){
-  // unhide and focus share screen
-  console.log( "nav share clicked");
-}
-function loginClick(){
-  // unhide and focus login screen
-  console.log("nav login clicked");
-}
-function logoutClick(){
-  // logout user, change welcome
-  console.log("nav logout clicked");
-}
-function registerClick(){
-  // unhide and focus register screen
-  console.log("nav register clicked");
-}
+let hashRoute = undefined; // explicit paranioa
+let userObj = undefined;
+let user = undefined;
+init();
 
-
-// TODO: team-front needs to slide down the distance of the team-back element behind it
-// figure this out on the fly or it looks weird ... 
-// pull array of 'our-team-main', lookup height of 'team-back' and set it to the slide of 'team-front'
-// ug fails ... tehy all need their own class ... lets try it a different way
-let teamMain = document.getElementsByClassName("our-team-main");
-let average = 0;
-for( const div of teamMain ){
-	// console.log( div );
-  // div.addEventListener("onmouseover", moveDiv());
-  // div.addEventListener("onmouseout", fixDiv());
-	// let height = div.children[1].getBoundingClientRect().height;
-	// average += height;
+function init() {
+	// let welcome = document.getElementById("login-welcome");
+	// welcome.innerHTML = "Welcome, Guest!";
+	// let logoutButton = document.getElementById("logout");
+	// logoutButton.addEventListener("click", logout);
 }
-// console.log("-" + (average / teamMain.length).toFixed(0) + "px");
-function moveDiv(div){
-  // console.log( "moveDiv", div );
-  // let newTop = 250;
-  // console.log(
-	// 	"moveDiv fired",
-	// 	div,
-	// 	"- " + div.children[0].style.top + " -",
-	// 	newTop + "px"
-	// );
-  // console.log(div.children[0].style);
-  // return;
-  // var newTop = div.children[0].style.top;
-  // newTop = parseInt(newTop);
-  // newTop -= 200;
-  // div.children[0].style.top = "250px";
-  // div.children[0].style.transition = "all 1s ease 1s";
+function render(html) {
+  document.getElementById("container").innerHTML = html;
 }
-function fixDiv(div){
-  // console.log( "fixDiv", div );
-	// let newTop = 0;
-  // console.log(
-	// 	"fixDiv fired",
-	// 	div,
-	// 	"- " + div.children[0].style.top + " -",
-	// 	newTop + "px"
-	// );
-  // console.log( div.children[0].style );
-  // return;
-  // var newTop = div.children[0].style.top;
-	// newTop = parseInt(newTop);
-	// newTop += 200;
-	// div.children[0].style.top = "";
-  // div.children[0].style.transition = "all 1s ease 1s";
-  // console.log("fixDiv fired", div, div.children[0].style.top, newTop + "px");
+function getCurrent() {
+  return window.location.hash;
+}
+function getCatImgURL(category) {
+  switch (category) {
+    case "Vegetables and legumes/beans":
+      return "https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549__340.jpg";
+    case "Fruits":
+      return "https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029__340.jpg";
+    case "Grain Food":
+      return "https://cdn.pixabay.com/photo/2014/12/11/02/55/corn-syrup-563796__340.jpg";
+    case "Milk, cheese, eggs and alternatives":
+      return "https://image.shutterstock.com/image-photo/assorted-dairy-products-milk-yogurt-260nw-530162824.jpg";
+    case "Lean meats and poultry, fish and alternatives":
+      return "https://t3.ftcdn.net/jpg/01/18/84/52/240_F_118845283_n9uWnb81tg8cG7Rf9y3McWT1DT1ZKTDx.jpg";
+  }
+}
+function logout() {
+  userObj = undefined;
+  user = undefined;
+  window.location.hash = "";
+}
+function moveDiv(div) {
+}
+function fixDiv(div) {
 }
